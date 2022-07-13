148c59355eb834df723cfd1df81a1df5a82bfeeb
willschneider15 <willschneider15@gmail.com>
2022-07-13 19:10:38 -0400

    first draft small changes to ui

---
+++ src/views/dashboard/ForecastCashflow.tsx
@@ -0,0 +1,97 @@
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import Button from '@mui/material/Button'
+import { useTheme } from '@mui/material/styles'
+import CardHeader from '@mui/material/CardHeader'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import CardContent from '@mui/material/CardContent'
+
+// ** Icons Imports
+import DotsVertical from 'mdi-material-ui/DotsVertical'
+
+// ** Third Party Imports
+import { ApexOptions } from 'apexcharts'
+
+// ** Custom Components Imports
+import ReactApexcharts from 'src/@core/components/react-apexcharts'
+
+const WeeklyOverview = () => {
+  // ** Hook
+  const theme = useTheme()
+
+  const options: ApexOptions = {
+    chart: {
+      height: 350,
+      type: 'line',
+    },
+
+    stroke: {
+      width: 5,
+      curve: 'smooth'
+    },
+    xaxis: {
+      type: 'datetime',
+      categories: ['1/11/2000', '2/11/2000', '3/11/2000', '4/11/2000', '5/11/2000', '6/11/2000', '7/11/2000', '8/11/2000', '9/11/2000', '10/11/2000', '11/11/2000', '12/11/2000', '1/11/2001', '2/11/2001', '3/11/2001','4/11/2001' ,'5/11/2001' ,'6/11/2001'],
+      tickAmount: 10,
+      labels: {
+ 
+      }
+    },
+    title: {
+      text: 'Forecast',
+      align: 'left',
+      style: {
+        fontSize: "16px",
+        color: '#666'
+      }
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: 'dark',
+        gradientToColors: [ '#FDD835'],
+        shadeIntensity: 1,
+        type: 'horizontal',
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 100, 100, 100]
+      },
+    },
+    yaxis: {
+      min: 100000,
+      max: 400000
+    }
+  }
+
+  return (
+    <Card>
+      <CardHeader
+        title='Total Monthly NOI'
+        titleTypographyProps={{
+          sx: { lineHeight: '2rem !important', letterSpacing: '0.15px !important' }
+        }}
+        action={
+          <IconButton size='small' aria-label='settings' className='card-more-options' sx={{ color: 'text.secondary' }}>
+            <DotsVertical />
+          </IconButton>
+        }
+      />
+      <CardContent sx={{ '& .apexcharts-xcrosshairs.apexcharts-active': { opacity: 0 } }}>
+        <ReactApexcharts type='line' height={205} options={options} series={[{ data: [100000, 100300, 130010, 190000, 200000, 250000, 270000, 290000, 300000, 280000, 310000, 350000, 360000, 379000, 357000, 387900, 390000, 399999] }]} />
+        <Box sx={{ mb: 7, display: 'flex', alignItems: 'center' }}>
+          <Typography variant='h5' sx={{ mr: 4 }}>
+            5%
+          </Typography>
+          <Typography variant='body2'>Your NOI is 5% 😎 better compared to last month</Typography>
+        </Box>
+        <Button fullWidth variant='contained'>
+          Details
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default WeeklyOverview
